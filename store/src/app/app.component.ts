66662f756ae83507a2008d9f3e9b6aab98cba133
diogoraposo13 <diogo.raposo@live.com.pt>
2023-10-04 03:00:18 +0100

    Installing dependencies and project setup

---
+++ store/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+    <h1>Test</h1>
+  `,
+  styles: []
+})
+export class AppComponent {
+  title = 'store';
+}
