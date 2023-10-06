278530b6fca0ca19d46f8bd6f576507aadfe4618
diogoraposo13 <diogo.raposo@live.com.pt>
2023-10-06 17:04:02 +0100

    Filters component

---
+++ store/src/app/pages/home/components/filters/filters.component.ts
@@ -0,0 +1,19 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-filters',
+  templateUrl: 'filters.component.html',
+ 
+})
+export class FiltersComponent {
+  @Output() showCategory = new EventEmitter<string>();
+
+  categories=['shoes','sports'];
+
+
+  onShowCategory(category: string): void {
+    this.showCategory.next(category);
+  }
+
+
+}
